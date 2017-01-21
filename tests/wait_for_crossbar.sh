1979542db93340f16f622f87609ffb6909fe577b
Alessandro Lai <alessandro.lai85@gmail.com>
2017-01-21 04:00:05 +0100

    Wait for crossbar to be ready before launching the tests in the Travis build

--- tests/wait_for_crossbar.sh
+++ tests/wait_for_crossbar.sh
@@ -4,6 +4,7 @@ for COUNTER in `seq 1 35`
 do
     nc -z crossbar-bundle.dev 54321;
     if [ $? = 0 ]; then
+        echo "Crossbar seems to be up! Port 54321 open"
         exit 0;
     else
         echo "Waiting for Crossbar to be ready..."
