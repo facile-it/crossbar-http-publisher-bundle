9741098f15feb2892207869eb148a2137e505945
Alessandro Lai <alessandro.lai85@gmail.com>
2020-04-01 09:07:24 +0200

    Fix waiting script

--- tests/wait_for_crossbar.sh
+++ tests/wait_for_crossbar.sh
@@ -2,7 +2,7 @@
 
 for COUNTER in `seq 1 30`
 do
-    curl -X POST -H "Content-Type: application/json" -s http://crossbar.dev:54321/publish > /dev/null;
+    curl -X POST -H "Content-Type: application/json" -s http://localhost:54321/publish > /dev/null;
     if [ $? = 0 ]; then
         echo "Crossbar seems to be up and responding!"
         exit 0;
